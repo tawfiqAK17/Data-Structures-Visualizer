d878e8efd4191e14e5eb26868b85cb399fbaa479
tawfiq akdit <tawfiqakdir@gmail.com>
2024-08-09 15:36:30 +0000

    i use arch BTW

---
+++ build/CMakeFiles/DataStructuresVisualizer.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/DataStructures/Array.cpp" "CMakeFiles/DataStructuresVisualizer.dir/DataStructures/Array.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/DataStructures/Array.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/DataStructures/DataStructure.cpp" "CMakeFiles/DataStructuresVisualizer.dir/DataStructures/DataStructure.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/DataStructures/DataStructure.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/UI/ArrayVisualizer.cpp" "CMakeFiles/DataStructuresVisualizer.dir/UI/ArrayVisualizer.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/UI/ArrayVisualizer.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/UI/Button.cpp" "CMakeFiles/DataStructuresVisualizer.dir/UI/Button.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/UI/Button.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/UI/MainWindow.cpp" "CMakeFiles/DataStructuresVisualizer.dir/UI/MainWindow.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/UI/MainWindow.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/UI/NodeRect.cpp" "CMakeFiles/DataStructuresVisualizer.dir/UI/NodeRect.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/UI/NodeRect.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/UI/StaticText.cpp" "CMakeFiles/DataStructuresVisualizer.dir/UI/StaticText.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/UI/StaticText.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/UI/TextHolder.cpp" "CMakeFiles/DataStructuresVisualizer.dir/UI/TextHolder.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/UI/TextHolder.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/UI/Visualizer.cpp" "CMakeFiles/DataStructuresVisualizer.dir/UI/Visualizer.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/UI/Visualizer.cpp.o.d"
+  "/home/tawfiq/CppProjects/DataStructuresVisualizer/main.cpp" "CMakeFiles/DataStructuresVisualizer.dir/main.cpp.o" "gcc" "CMakeFiles/DataStructuresVisualizer.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/DataStructuresVisualizer.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructuresVisualizer.dir/DataStructures/Array.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/DataStructures/Array.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/DataStructures/DataStructure.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/DataStructures/DataStructure.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/ArrayVisualizer.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/ArrayVisualizer.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/Button.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/Button.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/MainWindow.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/MainWindow.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/NodeRect.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/NodeRect.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/StaticText.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/StaticText.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/TextHolder.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/TextHolder.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/Visualizer.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/UI/Visualizer.cpp.o.d"
+  "CMakeFiles/DataStructuresVisualizer.dir/main.cpp.o"
+  "CMakeFiles/DataStructuresVisualizer.dir/main.cpp.o.d"
+  "DataStructuresVisualizer"
+  "DataStructuresVisualizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructuresVisualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
